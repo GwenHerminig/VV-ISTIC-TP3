53ae3755455fefa17aad13e60d0484ebd0ac1991
hermine <hermine.hamono@etudiant.univ-rennes1.fr>
2023-03-16 20:47:26 +0100

    Question 4.2

--- code/tp3-date/src/main/java/fr/istic/vv/Date.java
+++ code/tp3-date/src/main/java/fr/istic/vv/Date.java
@@ -2,16 +2,89 @@ package fr.istic.vv;
 
 class Date implements Comparable<Date> {
 
-    public Date(int day, int month, int year) { }
+    private final int day;
+    private final int month;
+    private final int year;
+    public Date(int day, int month, int year) {
+        if (!isValidDate(day, month, year)) {
+            throw new IllegalArgumentException("Invalid date.");
+        }
+        this.day = day;
+        this.month = month;
+        this.year = year;
+    }
 
-    public static boolean isValidDate(int day, int month, int year) { return false; }
+    public static boolean isValidDate(int day, int month, int year) {
+        if (month < 1 || month > 12) {
+        return false;
+    }
+        if (day < 1 || day > getDaysInMonth(month, year)) {
+            return false;
+        }
+        return true;
+    }
 
-    public static boolean isLeapYear(int year) { return false; }
+    public static boolean isLeapYear(int year) {
+        if (year % 400 == 0) {
+        return true;
+        }
+        if (year % 100 == 0) {
+            return false;
+        }
+        return (year % 4 == 0);
+    }
 
-    public Date nextDate() { return null; }
+    private static int getDaysInMonth(int month, int year) {
+        switch (month) {
+            case 2:
+                return isLeapYear(year) ? 29 : 28;
+            case 4:
+            case 6:
+            case 9:
+            case 11:
+                return 30;
+            default:
+                return 31;
+        }
+    }
+    public Date nextDate() {
+        int nextDay = day + 1;
+        int nextMonth = month;
+        int nextYear = year;
+        if (nextDay > getDaysInMonth(month, year)) {
+            nextDay = 1;
+            nextMonth++;
+            if (nextMonth > 12) {
+                nextMonth = 1;
+                nextYear++;
+            }
+        }
+        return new Date(nextDay, nextMonth, nextYear);
+    }
 
-    public Date previousDate() { return null; }
+    public Date previousDate() {
+        int previousDay = day - 1;
+        int previousMonth = month;
+        int previousYear = year;
+        if (previousDay == 0) {
+            previousMonth--;
+            if (previousMonth == 0) {
+                previousMonth = 12;
+                previousYear--;
+            }
+            previousDay = getDaysInMonth(previousMonth, previousYear);
+        }
+        return new Date(previousDay, previousMonth, previousYear);
+    }
 
-    public int compareTo(Date other) { return 0; }
+    public int compareTo(Date other) {
+        if (this.year != other.year) {
+            return Integer.compare(this.year, other.year);
+        }
+        if (this.month != other.month) {
+            return Integer.compare(this.month, other.month);
+        }
+        return Integer.compare(this.day, other.day);
+    }
 
 }
--- code/tp3-date/src/test/java/fr/istic/vv/DateTest.java
+++ code/tp3-date/src/test/java/fr/istic/vv/DateTest.java
@@ -2,9 +2,115 @@ package fr.istic.vv;
 
 import org.junit.jupiter.api.Test;
 
+import static fr.istic.vv.Date.isLeapYear;
+import static fr.istic.vv.Date.isValidDate;
 import static org.junit.jupiter.api.Assertions.*;
 
 class DateTest {
+
+    //isValidDate
+        //Date ok
+        @Test
+        public void dateOK() {
+            assertTrue(isValidDate(12,12,1212));
+        }
+        //Date ok : année très grande
+        @Test
+        public void dateOKYear() {
+            assertTrue(isValidDate(12,12,121222222));
+        }
+        //Date pas ok : sur le mois
+        @Test
+        public void dateNotOKmonth() {
+            assertFalse(isValidDate(12,13,121222222));
+        }
+        //Date pas ok : sur le jour
+        @Test
+        public void dateNotOKday() {
+            assertFalse(isValidDate(32,12,121222222));
+        }
+
+    //isLeapYear
+        //isLeapYear 400 multiple
+        @Test
+        public void leapYearOk400() {
+            assertTrue(isLeapYear(2000));
+        }
+        //is not Leap Year 100 multiple
+        @Test
+        public void leapYearNotOk100() {
+            assertFalse(isLeapYear(1900));
+        }
+        // is Leap year classic but not 400 multiple
+        @Test
+        public void leapYearOk() {
+            assertTrue(isLeapYear(2004));
+        }
+        // is not leap year classic but not 100 multiple
+        @Test
+        public void leapYearNotOk() {
+            assertFalse(isLeapYear(2003));
+        }
+
+    //nextDate
+        //deux jours normaux
+        @Test
+        public void dayNextDate() {
+            var d1 = new Date(12,12,200);
+            var d2 = new Date(13,12,200);
+            assertEquals(d1.nextDate().compareTo(d2),0);
+        }
+        //deux mois différents
+        @Test
+        public void monthNextDate() {
+            var d1 = new Date(30,11,200);
+            var d2 = new Date(1,12,200);
+            assertEquals(d1.nextDate().compareTo(d2),0);
+        }
+        //deux années différentes
+        @Test
+        public void yearNextDate() {
+            var d1 = new Date(31,12,2000);
+            var d2 = new Date(1,1,2001);
+            assertEquals(d1.nextDate().compareTo(d2),0);
+        }
+
+    //previousDate
+    //deux jours normaux
+    @Test
+    public void dayPreviousDate() {
+        var d2 = new Date(12,12,200);
+        var d1 = new Date(13,12,200);
+        assertEquals(d1.previousDate().compareTo(d2),0);
+    }
+    //deux mois différents
+    @Test
+    public void monthPreviousDate() {
+        var d2 = new Date(30,11,200);
+        var d1 = new Date(1,12,200);
+        assertEquals(d1.previousDate().compareTo(d2),0);
+    }
+    //deux années différentes
+    @Test
+    public void yearPreviousDate() {
+        var d2 = new Date(31,12,2000);
+        var d1 = new Date(1,1,2001);
+        assertEquals(d1.previousDate().compareTo(d2),0);
+    }
+
+    @Test
+    public void throwException() {
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> {
+            new Date(31,13,2000);
+        });
+        String expectedMessage = "Invalid date.";
+        String actualMessage = exception.getMessage();
+
+        assertTrue(actualMessage.contains(expectedMessage));
+    }
+
+
+
 
 
 }
