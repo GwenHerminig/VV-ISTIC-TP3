b6ba55bb9101f4f4b04e092d6e004039e5dea54a
hermine <hermine.hamono@etudiant.univ-rennes1.fr>
2023-03-16 19:08:26 +0100

    Question 3.1

--- code/tp3-balanced-strings/src/main/java/fr/istic/vv/StringUtils.java
+++ code/tp3-balanced-strings/src/main/java/fr/istic/vv/StringUtils.java
@@ -5,7 +5,43 @@ public class StringUtils {
     private StringUtils() {}
 
     public static boolean isBalanced(String str) {
-        return false;
-    }
+            int curlyBraces = 0, squareBrackets = 0, parentheses = 0;
+
+            for (int i = 0; i < str.length(); i++) {
+                char c = str.charAt(i);
+
+                switch (c) {
+                    case '{':
+                        curlyBraces++;
+                        break;
+                    case '}':
+                        curlyBraces--;
+                        if (curlyBraces < 0) {
+                            return false;
+                        }
+                        break;
+                    case '[':
+                        squareBrackets++;
+                        break;
+                    case ']':
+                        squareBrackets--;
+                        if (squareBrackets < 0) {
+                            return false;
+                        }
+                        break;
+                    case '(':
+                        parentheses++;
+                        break;
+                    case ')':
+                        parentheses--;
+                        if (parentheses < 0) {
+                            return false;
+                        }
+                        break;
+                }
+            }
+
+            return curlyBraces == 0 && squareBrackets == 0 && parentheses == 0;
+        }
 
 }
--- code/tp3-balanced-strings/src/test/java/fr/istic/vv/StringUtilsTest.java
+++ code/tp3-balanced-strings/src/test/java/fr/istic/vv/StringUtilsTest.java
@@ -7,5 +7,73 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class StringUtilsTest {
 
+    // empty string
+    @Test
+    public void testEmptyString() {
+        assertTrue(isBalanced(""));
+    }
 
+    // simple balanced symbols
+    @Test
+    public void testSimpleBalancedSymbols() {
+        assertTrue(isBalanced("{}"));
+        assertTrue(isBalanced("[]"));
+        assertTrue(isBalanced("()"));
+    }
+
+    // complexe balanced symbols
+    @Test
+    public void testComplexeBalancedSymbols() {
+        assertTrue(isBalanced("{[]}"));
+        assertTrue(isBalanced("[()]"));
+        assertTrue(isBalanced("{[()]}"));
+    }
+
+    // only opening symbols
+    @Test
+    public void testUnmatchedOpeningSymbols() {
+        assertFalse(isBalanced("{("));
+        assertFalse(isBalanced("("));
+    }
+
+    // only closing symbols
+    @Test
+    public void testUnmatchedClosingSymbols() {
+        assertFalse(isBalanced(")]}"));
+        assertFalse(isBalanced("}"));
+        assertFalse(isBalanced(")"));
+    }
+
+    // only opening symbols with one balanced pair
+    @Test
+    public void testUnmatchedOpeningWithOneBalancedSymbols() {
+        assertFalse(isBalanced("{[()"));
+    }
+
+    // only closing symbols with one balanced pair
+    @Test
+    public void testUnmatchedClosingWithOneBalancedSymbols() {
+        assertFalse(isBalanced("{]}"));
+        assertFalse(isBalanced("))))[)]"));
+        assertFalse(isBalanced("[]]"));
+    }
+    // more complexe balanced
+    @Test
+    public void testMixedSymbols() {
+        assertTrue(isBalanced("[{[()]}]"));
+        assertTrue(isBalanced("{[(])}"));
+    }
+    // separated complexe balanced symbols
+    @Test
+    public void testSeparatedMixedSymbols() {
+        assertTrue(isBalanced("{[()]}{[()]}"));
+        assertTrue(isBalanced("{[(])}{[()]}"));
+    }
+
+    // balanced pair with other characters
+    @Test
+    public void testOtherCharacters() {
+        assertTrue(isBalanced("abc[def]{ghi}jkl"));
+        assertTrue(isBalanced("a[b{c}d]e(f)g"));
+    }
 }
